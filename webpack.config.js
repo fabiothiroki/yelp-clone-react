009c476dcd67d352ecc10122bd486d64d4573320
fabiothiroki <fabiothiroki@gmail.com>
2018-05-29 21:13:01 -0300

    Basic project config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const root    = resolve(__dirname);
+const src     = join(root, 'src');
+const modules = join(root, 'node_modules');
+const dest    = join(root, 'dist');
+
+const NODE_ENV = process.env.NODE_ENV;
+const isDev = NODE_ENV === 'development';
+
+var config = getConfig({
+  isDev: isDev,
+  in: join(src, 'app.js'),
+  out: dest,
+  clearBeforeBuild: true
+})
+
+module.exports = config;
